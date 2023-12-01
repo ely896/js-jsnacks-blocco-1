c3c613f157c3f24da853aadff7b52d8ca4900710
ely896 <saretta2011@libero.it>
2023-12-01 12:40:09 +0100

    Add snack 4

--- Snack 4/scrip.js
+++ Snack 4/scrip.js
@@ -1,2 +1,21 @@
 /*In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, 
 chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.*/
+
+/*-array con lista invitati
+-prompt richiesta nome utente e definizione variabile
+-comparazione variabile nome utente con lista array*/
+
+
+const nameUser = prompt('inserisci il tuo nome');
+let partyList = ["Fabio", "Luigi", "Luca", "Davide", "Marco", "Lucia", "Giovanni", "Francesca",]
+
+for (let i = 0; i < partyList.length; i++) {
+    
+
+    if (nameUser === partyList[i]) {
+        console.log('puoi entrare');
+    } else {
+        console.log('non puoi entrare');
+    }
+    
+}
