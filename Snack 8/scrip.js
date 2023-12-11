63d21f4a4c13d46c0ce07d085693681e0f24c3e9
ely896 <saretta2011@libero.it>
2023-12-11 11:34:09 +0100

    Add Snack 8

--- Snack 8/scrip.js
+++ Snack 8/scrip.js
@@ -1,7 +1,23 @@
 /*Chiedi un numero di 4 cifre all’utente
 e calcola la somma di tutte le cifre che compongono il numero.*/
 
-const numberUser = Number(prompt('inserisci un numero di 4 cifre'));
+const numberUser = (prompt('inserisci un numero di 4 cifre'));
 
+console.log(numberUser);
+// considero il numero inserito come una stringa e uso la proprietà split per estrapolare singolarmente i 4 numeri
+numberUser.split("");
 
+console.log(numberUser.split(""));
+// con charAt proprietà della stringa definisco delle variabili prendendo i singoli numeri
+const a = numberUser.charAt(0);
+const b = numberUser.charAt(1);
+const c = numberUser.charAt(2);
+const d = numberUser.charAt(3);
+
+console.log(a, b, c, d);
+
+//con la variabile somma trasformo le stringhe di numeri in numeri grazie a number e ne faccio la somma
+let somma = Number (a) + Number (b) + Number (c) + Number (d);
+
+console.log(somma);
 
